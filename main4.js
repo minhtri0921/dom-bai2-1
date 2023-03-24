863c647c2ae94b4768e798af40ee6f8a28a52c23
minhtri0921 <113036460+minhtri0921@users.noreply.github.com>
2023-03-24 20:00:21 +0700

    bt

---
+++ main4.js
@@ -0,0 +1,137 @@
+let heading = ` <tr>
+<th>ID</th>
+<th>Tên hoa</th>
+<th>Loại hoa</th>
+<th>Hình ảnh</th>
+<th>Chức năng</th>
+</tr>`
+var listHoa = [];
+async function renderHoa() {
+    listHoa = await fetch('http://localhost:3004/listHoa')
+        .then(function (response) {
+            return response.json()
+        });
+
+    function render(flower) {
+        return `<tr>
+            <td>${flower.id}</td>
+            <td>${flower.tenHoa}</td>
+            <td>${flower.loaiHoa}</td>
+            <td><img src="${flower.hinhAnh}" alt="${flower.hinhAnh}" /></td>
+            <td>
+                            <a onclick="update(${flower.id})" href="#" title="Sửa"><img src="images/pencil.gif" alt="pencil.gif" />Sửa</a>
+                            <a onclick="remove(${flower.id})" href="#" title="Xóa"><img src="images/bin.gif" alt="bin.gif" />Xóa</a>
+                        </td>
+            </tr>`
+    }
+    function renderFlowers(listHoa) {
+        let elementTable = document.querySelector('table')
+        let str = ''
+        for (const flower of listHoa) {
+            str += render(flower)
+        }
+        elementTable.innerHTML = heading + str
+    }
+    renderFlowers(listHoa)
+}
+renderHoa()
+async function remove(id) {
+    console.log(id);
+    var result = confirm('Bạn có muốn xóa hoa này ? ')
+    if (result === true) {
+        // listcourses.splice(index, name)
+        var options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }
+        await fetch('http://localhost:3004/listHoa' + '/' + id, options)
+            .then(function (response) {
+                return response.json();
+            });
+    } else {
+        renderHoa()
+    }
+    renderHoa()
+}
+async function add() {
+    document.querySelectorAll("label").forEach(label => {
+        label.style.display = "block";
+    });
+    document.querySelectorAll("input").forEach(input => {
+        input.style.display = "block";
+    });
+    document.querySelectorAll("button").forEach(button => {
+        button.style.display = "block";
+    });
+    
+    let btnEl = document.getElementById('addFlower')
+    btnEl.onclick = async function(){
+        let newName = document.getElementById('name').value
+        let newType = document.getElementById('type').value
+        
+        var formData = {
+            tenHoa: newName,
+            loaiHoa: newType,
+            hinhAnh:"images/tmp/hoa1.jpg"
+        }
+        var options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        }
+    
+        await fetch('http://localhost:3004/listHoa', options)
+            .then(function (response) {
+                return response.json();
+            });
+    
+        renderHoa()
+    }
+}
+
+async function update(id){
+    let a = listHoa.find(function(hoa){
+        return hoa.id === id
+    })
+    document.querySelectorAll("label").forEach(label => {
+        label.style.display = "block";
+    });
+    document.querySelectorAll("input").forEach(input => {
+        input.style.display = "block";
+    });
+    let input = document.querySelectorAll('input')
+    input[0].value = a.tenHoa
+    input[1].value = a.loaiHoa
+    let btnUpdate = document.getElementById('updateFlower')
+    btnUpdate.style.display='block'
+    btnUpdate.onclick = async function () {
+        let name = document.getElementById('name').value
+        let type = document.getElementById('type').value
+
+        var formData = {
+            tenHoa: name,
+            loaiHoa: type,
+            hinhAnh : "images/tmp/hoa1.jpg"
+        }
+
+        var options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        }
+
+        await fetch('http://localhost:3004/listHoa' + "/" + id, options)
+            .then(function (response) {
+                return response.json();
+            });
+            document.getElementById('name').value = ''
+            document.getElementById('type').value = ''
+        renderHoa()
+    }
+}
